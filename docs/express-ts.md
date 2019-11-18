22127165505a1c1ff76ea1e4de53edf92d3ae406
zhangran22 <zhangran22@meituan.com>
2019-11-18 08:56:49 +0800

    增加一点typescript

---
+++ docs/express-ts.md
@@ -0,0 +1,4 @@
+
+`npx tsconfig.json` 初始化tsconfig.json文件
+`brew install mongodb` 安装mongodb
+
--- express-ts/app.ts
+++ express-ts/app.ts
@@ -1,1 +1,11 @@
-console.log(10);
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
+
+app.get("/", (_req: Request, res: Response) => {
+    res.send('Hello world1');
+});
+
+app.listen(3001, () => {
+    console.log('listen: 3001');
+});
