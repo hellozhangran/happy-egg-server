f17c89858e1f5c395ea272cc5e3e05d4ec234549
ran <hellozhangran@hotmail.com>
2019-12-10 23:39:22 +0800

    :art:

--- demos/app.js
+++ demos/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+var cluster = require('cluster');
 app.use('/api', (req, res, next) => {
+    console.log('hello api', cluster.isMaster);
     res.json('hello i am api');
 });
 app.listen('3000', () => {
