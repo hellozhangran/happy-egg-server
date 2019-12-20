9a8c3f0f25786708cc99b94078f034398167b213
ran <hellozhangran@hotmail.com>
2019-12-20 09:07:54 +0800

    test

--- h5/sources-server.js
+++ h5/sources-server.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const fs=require("fs");
 const app = express();
-app.use(express.static("public"));
+// 从根目录开始的
+app.use('/static', express.static("h5/src"));
 
-// 使用fs.readFile打开html文件
-app.get("/hello.html", function(request, response) {
-   fs.readFile("./"+request.path.substr(1),function(err,data){
-        // body
-        if(err){
-            console.log(err);
-            //404：NOT FOUND
-            response.writeHead(404,{"Content-Type":"text/html"});
-        }
-        else{
-            //200：OK
-            response.writeHead(200,{"Content-Type":"text/html"});
-            response.write(data.toString());
-        }
-        response.end();
-    });
-});
 app.listen(3000, function() {   //监听http://127.0.0.1:3000端口
     console.log("server start");
 });
