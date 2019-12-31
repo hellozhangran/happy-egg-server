e4b56d8ecd68aca07d1fd79ce8edc061e7f76efd
ran <hellozhangran@hotmail.com>
2019-12-31 08:55:05 +0800

    调整每小时更新

---
+++ demos/limit.js
@@ -0,0 +1,24 @@
+/**
+ * 通过一个日志文件来控制定时任务
+ */
+const fs = require('fs');
+const path = require('path');
+const getTodayStr = require('../utils/date').getTodayStr;
+const getHMS = require('../utils/date').getHMS;
+let filePath = path.join(__dirname, '../../logs/crawler.log');
+function limit(cb) {
+    let log = fs.readFileSync(filePath, 'utf8');
+    // 得到第一行
+    let list = log.split('\n');
+    let date = list[0].split(' ')[0];
+    let currentDay = getTodayStr(); // 20100101
+    let hms = getHMS(); // 12:23:01
+    if (date === currentDay) {
+        list.unshift(currentDay + ' ' + hms + ' ' + 'false');
+        fs.writeFileSync(filePath, list.join('\n'), 'utf8');
+    } else {
+        list.unshift(currentDay + ' ' + hms + ' ' + 'true');
+        fs.writeFileSync(filePath, list.join('\n'), 'utf8');
+        cb && cb();
+    }
+}
