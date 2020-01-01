7624302ba6798ed47b79cc748b408b1458d4d033
ran <hellozhangran@hotmail.com>
2020-01-01 09:39:32 +0800

    配置时间

--- express/schedule/index.js
+++ express/schedule/index.js
@@ -4,7 +4,7 @@ var limit = require('./limit.js');
 // 可以按照cron的格式设置
 function runSchedule (cb) {
     // 每小时执行一次 
-    schedule.scheduleJob('0 0 * * * *', function () {
+    schedule.scheduleJob('0 0 10 * * *', function () {
         console.log('定时任务执行一次');
         limit(function () {
             cb();
